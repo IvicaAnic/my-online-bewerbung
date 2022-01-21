2166bedd892fcde124efaaf5fa4aa5ed1882ea71
IvicaAnic <68137929+IvicaAnic@users.noreply.github.com>
2022-01-21 03:39:51 +0100

    first commit

---
+++ demo/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { viewerType } from 'modules/document-viewer.component';
+import { getbaseUrl } from 'demo/utils';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styles: []
+})
+export class AppComponent {
+  viewers: { name: viewerType; docs: string[]; custom: boolean; acceptedUploadTypes: string; viewerUrl?: string }[] = [
+   
+    {
+      name: 'pdf', docs: [
+        `${getbaseUrl()}/assets/Ivica_Anic_Lebenslauf_.pdf`
+      ], custom: false, acceptedUploadTypes: 'application/pdf'
+    },
+    
+  ];
+  selectedViewer = this.viewers[0];
+  selectedDoc = this.selectedViewer.docs[0];
+
+  constructor() { }
+  selectViewer(viewerName: viewerType) {
+    if (viewerName !== this.selectViewer.name) {
+      this.selectedViewer = this.viewers.find(v => v.name === viewerName);
+      this.selectedDoc = this.selectedViewer.docs[0];
+    }
+  }
+
+  getDocExtension(doc: string) {
+    const splittedDoc = doc.split('.');
+    return splittedDoc[splittedDoc.length - 1];
+  }
+
+  handleFiles(fileInput: any) {
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.selectedDoc = e.target.result;
+      };
+      reader.readAsDataURL(fileInput.target.files[0]);
+    }
+  }
+
+}
